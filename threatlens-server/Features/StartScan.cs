8a34a050858a9cfa997f1db601d6d2a710db1c7d
Sead Xhigoli <sead.xhigoli1@gmail.com>
2024-12-17 22:04:17 +0100

    add nuget packages and base controller

---
+++ threatlens-server/Features/StartScan.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace threatlens_server.Features
+{
+    public class StartScan
+    {
+        [HttpPost("/api/start-scan")]
+        public async Task<ActionResult<int>> Create(StartScanCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+    public record StartScanCommand() : IRequest<int>;
+}
--- threatlens-server/Program.cs
+++ threatlens-server/Program.cs
@@ -1,6 +1,54 @@
+using Microsoft.OpenApi.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod()));
+
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "VSA Todo API", Version = "v1" }));
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddHealthChecks();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error-development");
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+//app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
 
 app.Run();
